b657589b42a05503768232beb1465da76890217b
Alexander <aguillenhernandez95@gmail.com>
2023-01-09 19:42:09 +0100

    Added style, structure and some functionality to the calculator

---
+++ script.js
@@ -0,0 +1,13 @@
+let currentScreen = document.getElementById("current"),
+    previousScreen = document.getElementById("previous"),
+    currentNum = "";
+    previousNum = "",
+    operation = ""
+    opSings = ["+", "-", "*", "/"];
+
+let getValue = (element) => {
+    
+    if (opSings.includes(element.value)) {
+
+    }
+}
